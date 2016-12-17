3147ab82c45354a0a9c36e867c8c888025fa183b
Yuri M. Clene <costavold145@outlook.com>
2016-12-17 09:54:06 -0200

    Update hexobjdmp.sh

--- hexobjdmp.sh
+++ hexobjdmp.sh
@@ -4,7 +4,7 @@
 clear
  echo -n "\033[00;36mBy vold\n"
  sleep 1
-    echo -n "\033[00;36mInsira o arquivo\n\n";
+    echo -n "\033[00;36mInsira o arquivo\n\n"
 read -p "digite o arquivo: " file
  objdump -d $file | grep '[0-9a-f]:' | grep -v file | cut -f2 -d':' | cut -f1-2 -d' ' | tr -s ' ' | tr '\t' ' ' | sed 's/ $//' | sed 's/ /\\x/g' | paste -d '' -s | sed 's/^/"/' | sed 's/$/"/'
 echo -n "\n\n\n"
