9b0212dcd2ca484b3646ae18ee9492fede899579
Yuri M. Clene <costavold145@outlook.com>
2016-12-17 09:50:46 -0200

    Create hexobjdmp.sh

---
+++ hexobjdmp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#hexobjdmp.sh
+#gera só o hex opcode
+clear
+ echo -n "\033[00;36mBy vold\n"
+ sleep 1
+    echo -n "\033[00;36mInsira o arquivo\n\n";
+read -p "digite o arquivo: " file
+ objdump -d $file | grep '[0-9a-f]:' | grep -v file | cut -f2 -d':' | cut -f1-2 -d' ' | tr -s ' ' | tr '\t' ' ' | sed 's/ $//' | sed 's/ /\\x/g' | paste -d '' -s | sed 's/^/"/' | sed 's/$/"/'
+echo -n "\n\n\n"
+   echo -n "\033[00;32m\tTsunami Faction s2\n"
+sleep 1
+exit
