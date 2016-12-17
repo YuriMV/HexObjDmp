79966a040216bf81d01b60e8026e1ccf358a6d87
Yuri M. Clene <costavold145@outlook.com>
2016-12-17 10:59:21 -0200

    Update hexobjdmp.sh

--- hexobjdmp.sh
+++ hexobjdmp.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #hexobjdmp.sh
 #gera só o hex opcode
+#uso: sh hexobjdmp.sh arquivo
 echo 'unsigned const char sc[]='
  objdump -d $1 | grep '[0-9a-f]:' | grep -v file | cut -f2 -d':' | cut -f1-2 -d' ' | tr -s ' ' | tr '\t' ' ' | sed 's/ $//' | sed 's/ /\\x/g' | paste -d '' -s | sed 's/^/"/' | sed 's/$/"/'
 echo ';'
