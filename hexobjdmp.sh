06471d9f17b3c6272d546356653afd90ca02d0a5
Yuri M. Clene <costavold145@outlook.com>
2017-02-25 08:45:28 -0300

    Update hexobjdmp.sh

--- hexobjdmp.sh
+++ hexobjdmp.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 #hexobjdmp.sh
 #gera só o hex opcode
-echo 'unsigned const char sc[]='
+if [ -z $1 ]
+then
+ printf "\e[0;31mUso: bash $0 arquivo\e[0m"
+else
+ echo 'unsigned const char sc[]='
  objdump -d $1 | grep '[0-9a-f]:' | grep -v file | cut -f2 -d':' | cut -f1-2 -d' ' | tr -s ' ' | tr '\t' ' ' | sed 's/ $//' | sed 's/ /\\x/g' | paste -d '' -s | sed 's/^/"/' | sed 's/$/"/'
 echo ';'
 echo ""
    printf "\e[00;32m\tTsunami Faction s2\n\e[0m"
-
+fi
 
