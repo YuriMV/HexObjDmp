36117d4e9c8736f92c579a7e118f5169405bf218
Yuri M. Clene <costavold145@outlook.com>
2017-02-25 08:40:27 -0300

    Update hexobjdmp.sh

--- hexobjdmp.sh
+++ hexobjdmp.sh
@@ -4,7 +4,7 @@
 echo 'unsigned const char sc[]='
  objdump -d $1 | grep '[0-9a-f]:' | grep -v file | cut -f2 -d':' | cut -f1-2 -d' ' | tr -s ' ' | tr '\t' ' ' | sed 's/ $//' | sed 's/ /\\x/g' | paste -d '' -s | sed 's/^/"/' | sed 's/$/"/'
 echo ';'
-echo -n "\n\n"
-   echo -n "\033[00;32m\tTsunami Faction s2\n"
+echo ""
+   printf "\e[00;32m\tTsunami Faction s2\n\e[0m"
 
 
